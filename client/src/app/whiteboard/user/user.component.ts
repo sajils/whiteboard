66fd42c78baf3064b4c3874e51b9a4efc3917677
Sajil <sajilgopinathan@gmail.com>
2018-09-27 13:45:23 +0530

    removed unwanted codes.

--- client/src/app/whiteboard/user/user.component.ts
+++ client/src/app/whiteboard/user/user.component.ts
@@ -1,29 +1,21 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'tcc-dialog-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
-  usernameFormControl = new FormControl('', [Validators.required]);
-  previousUsername: string;
 
-  constructor(
-    @Inject(MAT_DIALOG_DATA) public params: any) {
-    this.previousUsername = params.username ? params.username : undefined;
+export class UserComponent implements OnInit {
+
+ 
+
+  constructor() {
+ 
   }
 
   ngOnInit() {
   }
   
-  public onSave(): void { 
-    // this.dialogRef.close({
-    //   username: this.params.username,
-    //   dialogType: this.params.dialogType,
-    //   previousUsername: this.previousUsername
-    // });
   }
 }
